4920f9967dd7a84fc39204256f9e74787643228d
Panchito <j99crispin@gmail.com>
2025-05-23 13:33:56 -0500

    cambio

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+// const mockTodos = [
+//   {
+//     id: 1,
+//     title: 'todo1',
+//     completed: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'todo2',
+//     completed: true,
+//   },
+//   {
+//     id: 3,
+//     title: 'todo3',
+//     completed: false,
+//   }
+// ]
+
+
+const App: React.FC = () => {
+  return (
+    <h1>todo mvc</h1>
+  )
+}
+
+export default App
